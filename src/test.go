b23583a2082469c14aba749f8d9dd07d21e72189
unknown <524502256@qq.com>
2020-06-20 10:56:25 +0800

    6.19打卡

--- src/test.go
+++ src/test.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2}
-	fmt.Println(a[:len(a)-1])
+	a := new(int)
+	*a = 123
+	fmt.Println(*a)
 }
---
+++ src/剑指offer/剑指 Offer 32 - I. 从上到下打印二叉树.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+func levelOrder(root *TreeNode) []int {
+	if root == nil {
+		return  nil
+	}
+	queue := []*TreeNode{}
+	res := []int{}
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		if root.Left != nil {
+			queue = append(queue, root.Left)
+		}
+
+		if root.Right != nil {
+			queue = append(queue, root.Right)
+		}
+		res = append(res, root.Val)
+		fmt.Println(root)
+		queue = queue[1:]
+		if len(queue) != 0 {
+			root = queue[0]
+		}
+	}
+
+	return res
+}
